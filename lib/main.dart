dfc034c9e22f62c6a24406313a41105884357093
phartoonax <phartoonax@gmail.com>
2023-02-15 20:11:31 +0700

    done

--- lib/main.dart
+++ lib/main.dart
@@ -23,6 +23,12 @@ const String ANDROID_API_KEY = "37EE5605-1A6E-45FE-B475-90CAFBB5D2A1";
 const String IOS_API_KEY = "868DBCE3-EAE9-436E-AD67-557A5ADF44C7";
 const String JS_API_KEY = "11EFD95E-1E59-4853-8E0F-D3A0ADEED12D";
 
+//BACKUP SERVER
+// const String APPLICATION_ID = "458084E0-EA3D-0CAB-FFB9-7944218CCA00";
+// const String ANDROID_API_KEY = "9A1322D9-F4FC-4900-B9EB-4A786E039307";
+// const String IOS_API_KEY = "A831EF88-FAC2-47EA-B170-E8EBC086B034";
+// const String JS_API_KEY = "3701441A-4180-47CD-BF96-237A9001A778";
+
 class _MyAppState extends State<MyApp> {
   @override
   initState() {
@@ -37,7 +43,8 @@ class _MyAppState extends State<MyApp> {
       iosApiKey: IOS_API_KEY,
       jsApiKey: JS_API_KEY,
     );
-    await Backendless.setUrl(SERVER_URL);
+    await Backendless.setUrl(
+        SERVER_URL); //TODO: REENABLE THIS WHEN SERVER IS BACK
   }
 
   // This widget is the root of your application.
